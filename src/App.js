471f572937f05d9264121283f07e900de45baae8
karth <karthiknaveen22022002@gmail.com>
2022-05-02 17:17:28 +0530

    created components and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import SideNav from "./Components/SideNav";
+import Home from "./Pages/Home"
+import AboutMe from "./Pages/AboutMe"
+import Resume from "./Pages/Resume"
+import Github from "./Pages/Github"
+import Services from "./Pages/Services"
+import Projects from "./Pages/Projects"
+
+
+
+const AppLayout = styled.div`
+display: flex;
+
+
+`
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AppLayout>
+          <SideNav/>
+
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="aboutMe" element={<AboutMe />}></Route>
+            <Route path="projects" element={<Projects />}></Route>
+            <Route path="resume" element={<Resume />}></Route>
+            <Route path="github" element={<Github />}></Route>
+            <Route path="services" element={<Services />}></Route>
+          </Routes>
+        </AppLayout>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/SideNav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container=styled.div`
+	
+	display: flex;
+	flex-direction: column;
+	background-color: red;
+`
+
+
+const SideNav = () => {
+		return (
+      <Container>
+        
+      </Container>
+    );
+}
+
+export default SideNav
