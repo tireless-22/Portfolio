28ff354964b1f4eb694e8d3079bf07dde2a4d215
karth <karthiknaveen22022002@gmail.com>
2022-05-02 17:44:42 +0530

    toggle fucntionality added to the sidenav in web view

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-const Container=styled.div`
-	
-	display: flex;
-	flex-direction: column;
-	background-color: red;
-`
+import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 10px;
+`;
+const ToggleOn = styled.div`
+  display: flex;
+  width: 250px;
+  height: calc(100vh - 56px);
+  flex-direction: column;
+  background-color: red;
+`;
+const ToggleOff = styled.div`
+  display: flex;
+  width: 80px;
+  height: calc(100vh - 56px);
+  flex-direction: column;
+  background-color: blue;
+`;
 
 const SideNav = () => {
-		return (
-      <Container>
-        
-      </Container>
-    );
-}
+  const [toggle, setToggle] = useState(true);
 
-export default SideNav
+  return (
+    <Container>
+      {toggle ? (
+        <AiOutlineMenuFold size="40px" onClick={() => setToggle(!toggle)} />
+      ) : (
+        <AiOutlineMenuUnfold size="40px" onClick={() => setToggle(!toggle)} />
+      )}
+
+      {toggle ? (
+        <ToggleOn>
+          
+        </ToggleOn>
+      ) : (
+        <ToggleOff>
+         
+        </ToggleOff>
+      )}
+    </Container>
+  );
+};
+
+export default SideNav;
