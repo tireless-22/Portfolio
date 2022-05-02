950c5acbb8b5a0497f5f7c858e89af61d1387c9d
karth <karthiknaveen22022002@gmail.com>
2022-05-02 21:10:34 +0530

    updated the nav

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -2,25 +2,117 @@ import React from "react";
 import styled from "styled-components";
 import { useState } from "react";
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
+import Naveen from "../Images/SideNav/Naveen.png";
+import { Link } from "react-router-dom";
+
+import {
+  SiHackerrank,
+  SiLeetcode,
+  SiCodechef,
+  SiCodeforces,
+  SiLinkedin,
+  SiGithub,
+} from "react-icons/si";
+
+import {AiOutlineHome} from "react-icons/ai"
+
+import { CgProfile } from "react-icons/cg";
+import {AiOutlineFundProjectionScreen} from "react-icons/ai"
+import {GrDocumentUser} from "react-icons/gr"
+import { AiFillGithub } from "react-icons/ai"
+import {GrServices} from "react-icons/gr"
+
+
+
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   padding-left: 10px;
+  padding-right: 10px;
+  background-color: antiquewhite;
+  border: 2px solid black;
+		border-radius: 5px;
 `;
 const ToggleOn = styled.div`
   display: flex;
   width: 250px;
   height: calc(100vh - 56px);
   flex-direction: column;
-  background-color: red;
+
 `;
+
+const ImageContainer = styled.div`
+display: flex;
+justify-content: center;
+margin-top: 10px;
+
+`
+
+const Image=styled.img`
+	
+	width:160px;
+	height: 180px;
+	border-radius: 50%;
+`
+
+const SocialMediaContiner=styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	margin-top: 15px;
+
+
+`
+
+const RouteButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+
+`;
+
+const RouterButtonContainer = styled.div`
+
+  display: flex;
+  margin-top:10px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border-radius: 4px;
+  /* border:black 2px solid; */
+  
+
+`;
+
+const RouterIcon = styled.div`
+margin-right:10px;
+margin-left: 0px;
+  
+
+`
+
+
+const RouterHeading = styled.div`
+  font-size: 35px;
+  color: black;
+  text-decoration: none;
+`;
+
+
+	
+
+
 const ToggleOff = styled.div`
   display: flex;
   width: 80px;
   height: calc(100vh - 56px);
   flex-direction: column;
-  background-color: blue;
+
+  /* background-color: blue; */
 `;
 
 const SideNav = () => {
@@ -36,12 +128,86 @@ const SideNav = () => {
 
       {toggle ? (
         <ToggleOn>
-          
+          <ImageContainer>
+            <Image src={Naveen}></Image>
+          </ImageContainer>
+          <SocialMediaContiner>
+            <SiLinkedin size="35px" />
+            <SiGithub size="35px" />
+            <SiLeetcode size="35px" />
+          </SocialMediaContiner>
+          <SocialMediaContiner>
+            <SiCodechef size="35px" />
+            <SiHackerrank size="35px" />
+            <SiCodeforces size="35px" />
+          </SocialMediaContiner>
+
+          <RouteButtonsContainer>
+            <Link to="/">
+              <RouterButtonContainer>
+                <RouterIcon>
+                  <AiOutlineHome color="black" size="35px" />
+                </RouterIcon>
+
+                <RouterHeading>Home</RouterHeading>
+
+                {/* <RouteButton>Home</RouteButton> */}
+              </RouterButtonContainer>
+            </Link>
+            <Link to="aboutMe">
+              <RouterButtonContainer>
+                <RouterIcon>
+                  <CgProfile color="black" size="35px" />
+                </RouterIcon>
+                <RouterHeading> About Me</RouterHeading>
+
+                {/* <RouteButton>About Me</RouteButton> */}
+              </RouterButtonContainer>
+            </Link>
+            <Link to="projects">
+              <RouterButtonContainer>
+                <RouterIcon>
+                  <AiOutlineFundProjectionScreen color="black" size="35px" />
+                </RouterIcon>
+                <RouterHeading>Projects</RouterHeading>
+
+                {/* <RouteButton>Projects</RouteButton> */}
+              </RouterButtonContainer>
+            </Link>
+            <Link to="resume">
+              <RouterButtonContainer>
+                <RouterIcon>
+                  <GrDocumentUser color="black" size="35px" />
+                </RouterIcon>
+                <RouterHeading>Resume</RouterHeading>
+
+                {/* <RouteButton>Resume</RouteButton> */}
+              </RouterButtonContainer>
+            </Link>
+            <Link to="github">
+              <RouterButtonContainer>
+                <RouterIcon>
+                  <AiFillGithub color="black" size="35px" />
+                </RouterIcon>
+                <RouterHeading>GitHub</RouterHeading>
+
+                {/* <RouteButton>GitHub</RouteButton> */}
+              </RouterButtonContainer>
+            </Link>
+            <Link to="services">
+              <RouterButtonContainer>
+                <RouterIcon>
+                  <GrServices color="black" size="35px" />
+                </RouterIcon>
+                <RouterHeading>Services</RouterHeading>
+
+                {/* <RouteButton>Services</RouteButton> */}
+              </RouterButtonContainer>
+            </Link>
+          </RouteButtonsContainer>
         </ToggleOn>
       ) : (
-        <ToggleOff>
-         
-        </ToggleOff>
+        <ToggleOff></ToggleOff>
       )}
     </Container>
   );
